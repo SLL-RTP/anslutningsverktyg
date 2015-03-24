8e4b56bbe56cf0ac665593c5087d9de318f8f352
Martin Samuelsson <commit@martsam.se>
2015-03-24 11:30:08 +0100

    do not mock installed status in service contract factory

--- app/scripts/factories/serviceContract.js
+++ app/scripts/factories/serviceContract.js
@@ -2,38 +2,43 @@
 angular.module('avApp')
   .factory('ServiceContract', ['$q', '$http', 'configuration',
     function ($q, $http, configuration) {
-    return {
-      listContracts: function(serviceComponentId, environmentId, serviceDomainId) {
-        console.log('serviceComponentId: ' + serviceComponentId + ', environmentId: ' + environmentId + ', serviceDomainId: ' + serviceDomainId);
-        var deferred = $q.defer();
-        $http.get(configuration.apiHost + '/anslutningsplattform/api/serviceContracts', {
-          params: {
-            hsaId: serviceComponentId,
-            environmentId: environmentId,
-            serviceDomainId: serviceDomainId
-          }
-        }).success(function(data) {
-          var serviceContracts = _.map(data, function(serviceContract) {
-            if (!!Math.floor(Math.random() * 2)) {
-              serviceContract.installedInEnvironment = true;
-              serviceContract.installedForProducerHsaId = true;
-            } else if (!!Math.floor(Math.random() * 2)) {
-              serviceContract.installedInEnvironment = true;
-              serviceContract.installedForProducerHsaId = false;
-            } else {
-              serviceContract.installedInEnvironment = false;
-              serviceContract.installedForProducerHsaId = false;
+      var serviceContractFactory = {
+        listContracts: function (serviceComponentId, environmentId, serviceDomainId) {
+          console.log('serviceComponentId: ' + serviceComponentId + ', environmentId: ' + environmentId + ', serviceDomainId: ' + serviceDomainId);
+          var deferred = $q.defer();
+          $http.get(configuration.apiHost + '/anslutningsplattform/api/serviceContracts', {
+            params: {
+              hsaId: serviceComponentId,
+              environmentId: environmentId,
+              serviceDomainId: serviceDomainId
             }
-            //serviceContract.installedInEnvironment = !!Math.floor(Math.random() * 2);
-            //serviceContract.installedForProducerHsaId = !!Math.floor(Math.random() * 2);
-            serviceContract.serviceDomainId = serviceDomainId;
-            return serviceContract;
+          }).success(function (data) {
+            var serviceContracts = _.map(data, function (serviceContract) {
+              serviceContract.serviceDomainId = serviceDomainId;
+              return serviceContract;
+              //return _mockInstalledStatus(serviceContract);
+            });
+            deferred.resolve(serviceContracts);
+          }).error(function (data, status, headers) { //TODO: handle errors
+            deferred.reject();
           });
-          deferred.resolve(serviceContracts);
-        }).error(function(data, status, headers) { //TODO: handle errors
-          deferred.reject();
-        });
-        return deferred.promise;
-      }
-    };
+          return deferred.promise;
+        }
+      };
+
+      var _mockInstalledStatus = function(serviceContract) {
+        if (!!Math.floor(Math.random() * 2)) {
+          serviceContract.installedInEnvironment = true;
+          serviceContract.installedForProducerHsaId = true;
+        } else if (!!Math.floor(Math.random() * 2)) {
+          serviceContract.installedInEnvironment = true;
+          serviceContract.installedForProducerHsaId = false;
+        } else {
+          serviceContract.installedInEnvironment = false;
+          serviceContract.installedForProducerHsaId = false;
+        }
+        return serviceContract;
+      };
+
+      return serviceContractFactory;
   }]);
